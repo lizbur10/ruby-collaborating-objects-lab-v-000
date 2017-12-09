545c382c86f627690c5406eca498e84229afbf1a
Liz Burton <liz.burton147@gmail.com>
2017-12-09 15:40:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,9 @@ class Song
   end
 
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
-
+    artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+    @song.artist = artist
   end
 
 end
