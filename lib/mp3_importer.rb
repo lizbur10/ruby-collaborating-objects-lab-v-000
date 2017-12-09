184682476155daed8fe224e74bb762418994e269
Liz Burton <liz.burton147@gmail.com>
2017-12-09 12:22:07 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,3 +16,19 @@ class MP3Importer
   end
 
 end
+
+
+artist = Artist.new('Michael Jackson')
+song_one = Song.new("Rock With You")
+song_two = Song.new("Smooth Criminal")
+artist_1 = Artist.find_or_create_by_name("Michael Jackson")
+artist_2 = Artist.find_or_create_by_name("Michael Jackson")
+artist_1 = Artist.find_or_create_by_name("Drake")
+dirty_diana = Song.new("Dirty Diana")
+billie_jean = Song.new("Billie Jean")
+artist.add_song(dirty_diana)
+artist.add_song(billie_jean)
+song = Song.new('Man in the Mirror')
+file_name = 'Michael Jackson - Black or White - pop.mp3'
+song.name = 'Thriller'
+new_instance = Song.new_by_filename(file_name)
