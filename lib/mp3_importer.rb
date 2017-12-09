85ce77083b304c6873633cb2432ff79009c265b7
Liz Burton <liz.burton147@gmail.com>
2017-12-09 16:15:26 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,5 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path, :files
 
   def initialize(path)
     @path = path
@@ -7,14 +7,14 @@ class MP3Importer
 
   def files
     #load the files in the directory indicated by the path
-    files = Dir["#{@path}/*.mp3"]
+    @files = Dir["#{@path}/*.mp3"]
     #normalizes the filename (removes the path)
-    files.map { | file | file.gsub("#{path}/", "") }
+    @files.map { | file | file.gsub("#{path}/", "") }
   end
 
   def import
     #calls Song.new_by_filename which creates the song from the filename
-    Song.new_by_filename(file_name)
+    @files.each { | file_name | Song.new_by_filename(file_name)}
   end
 
 end
