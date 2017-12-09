2a3cc597e64a622a5f507537b6a191f80e3911bb
Liz Burton <liz.burton147@gmail.com>
2017-12-09 12:49:04 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,18 +18,23 @@ class MP3Importer
 end
 
 
-artist = Artist.new('Michael Jackson')
+michael = Artist.new('Michael Jackson')
 michael_song_one = Song.new("Rock With You")
-artist.add_song(song_one)
+michael.add_song(michael_song_one)
 michael_song_two = Song.new("Smooth Criminal")
-artist.add_song(song_two)
+michael.add_song(michael_song_two)
 
-artist = Artist.new('Adele')
-adele_song_one = Song.new("hello")
-artist.add_song(adele_song_one)
-adele_song_two = Song.new("")
-artist.add_song(adele_song_two)
+adele = Artist.new('Adele')
+adele_song_one = Song.new("Hello")
+adele.add_song(adele_song_one)
+adele_song_two = Song.new("Someone Like You")
+adele.add_song(adele_song_two)
+adele_song_three = Song.new("Rolling in the Deep")
+adele.add_song(adele_song_three)
 
+elvis = Artist.new("Elvis Costello")
+elton = Artist.new("Elton John")
+moby = Artist.new("Moby")
 
 artist_1 = Artist.find_or_create_by_name("Michael Jackson")
 artist_2 = Artist.find_or_create_by_name("Michael Jackson")
