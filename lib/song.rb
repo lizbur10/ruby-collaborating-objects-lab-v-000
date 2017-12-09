8857f0b326ccea003cdfa314203dada9102ec3cc
Liz Burton <liz.burton147@gmail.com>
2017-12-09 14:48:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,11 +9,13 @@ class Song
 
   def self.new_by_filename(file_name)
     artist_name = file_name.split(" - ")[0]
+    artist_name=(artist_name)
     song_name = file_name.split(" - ")[1]
   end
 
   def artist_name=(name)
-
+    Artist.find_or_create_by_name(name)
+    
   end
 
 end
